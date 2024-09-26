2bb1ddc7752bf2eaeff84ea6f99f32ea2faae248
Fbartoli <florent.b@safe.global>
2024-09-26 16:04:08 +0200

    fix import

--- lib/smartSession.ts
+++ lib/smartSession.ts
@@ -11,11 +11,13 @@ import {
 } from '@rhinestone/module-sdk'
 
 
-import { Address, Hex, PublicClient, encodeAbiParameters, encodeFunctionData, toBytes, toHex, zeroAddress } from 'viem'
-import { SafeSmartAccountClient, publicClient } from './permissionless';
+import { Address, Hex, PublicClient, encodeAbiParameters, encodeFunctionData, http, toBytes, toHex, zeroAddress } from 'viem'
+import { SafeSmartAccountClient, pimlicoUrl, publicClient } from './permissionless';
 import { privateKeyToAccount } from 'viem/accounts';
 import { abi } from './abi';
 import { sendUserOp } from './sendUserOp';
+import { createBundlerClient } from 'viem/account-abstraction';
+import { sepolia } from 'viem/chains';
 
 let privateKeySession = "0x5e6e0e7445eb80a4926c61730fb72ca0053f9db2621064534a349d75797af3ac" as `0x${string}`
 // if (!privateKeySession) {
